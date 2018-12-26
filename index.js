a6962c94bf0f9d6565444cb3db5589ced8bcf503
Lucia <lublasal@gmail.com>
2018-12-26 13:23:47 +0100

    Objects

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const net = new brain.NeuralNetwork({ hiddenLayers: [3] })
+////////// FIRST EXAMPLE /////////////////
+/* const net = new brain.NeuralNetwork({ hiddenLayers: [3] })
 
 const trainingData = [
   { input: [0, 0], output: [0] },
@@ -7,18 +8,68 @@ const trainingData = [
   { input: [1, 1], output: [0] },
 ];
 
-/*  
-  In train, it does:
-  Forward propagation -> predicts what it'll need
-  Backward propagation -> measures the error and learns 
-*/
+  // In train, it does:
+  // Forward propagation -> predicts what it'll need
+  // Backward propagation -> measures the error and learns 
+
 net.train(trainingData, {
-    log: (error) => console.log(error),
-    logPeriod: 100
+    // log: (error) => console.log(error),
+    // logPeriod: 100
   }
 );
 
 console.log(net.run([0,0]));
 console.log(net.run([0,1]));
 console.log(net.run([1,0]));
-console.log(net.run([1,1]));
+console.log(net.run([1,1]));*/
+
+
+/////// WORKING WITH OBJECTS //////////
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] })
+
+const colors = [
+  { green: 0.2, blue: 0.4 },
+  { green: 0.4, blue: 0.6 },
+  { red: 0.2, green: 0.8, blue: 0.8 },
+  { green: 1, blue: 1 },
+  { red: 0.8, green: 1, blue: 1 },
+  { red: 1, green: 1, blue: 1 },
+  { red: 1, green: 0.8, blue: 0.8 },
+  { red: 1, green: 0.6, blue: 0.6 },
+  { red: 1, green: 0.4, blue: 0.4 },
+  { red: 1, green: 0.31, blue: 0.31 },
+  { red: 0.8 },
+  { red: 0.6, green: 0.2, blue: 0.2 }
+];
+
+const brightnesses = [
+  { dark: 0.8 },
+  { neutral: 0.8 },
+  { light: 0.7 },
+  { light: 0.8 },
+  { light: 0.9 },
+  { light: 1 },
+  { light: 0.8 },
+  { neutral: 0.7, light: 0.5 },
+  { dark: 0.5, neutral: 0.5 },
+  { dark: 0.6, neutral: 0.3 },
+  { dark: 0.85 },
+  { dark: 0.9 }
+];
+
+const trainingData = []
+
+colors.forEach((color, index) => {
+  trainingData.push({
+    input: color,
+    output: brightnesses[index]
+  });
+});
+
+const stats = net.train(trainingData);
+
+console.log(stats);
+
+console.log(net.run({
+  red: 0.9
+}));
