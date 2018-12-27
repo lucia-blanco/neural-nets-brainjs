8c0282a39ff49b53043b6f753574159447b65a5d
Lucia <lublasal@gmail.com>
2018-12-27 13:22:34 +0100

    Stock market price predictor

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function restaurantForDay(dayOfWeek) {
 console.log(restaurantForDay('Monday'));*/
 
 /////////// COUNTING TO 5 ////////////////
-const trainingData = [
+/*const trainingData = [
   [1,2,3,4,5],
   [5,4,3,2,1]
 ];
@@ -136,5 +136,53 @@ const net = new brain.recurrent.LSTMTimeStep();
 net.train(trainingData);
 
 console.log(Math.round(net.run([1,2,3,4])));
-console.log(Math.round(net.run([5,4,3])));
+console.log(Math.round(net.run([5,4,3])));*/
+
+////////////// PREDICTING MULTIPLE STEPS ///////////////
+
+function scaleDown(step) { // normalize
+  return {
+    open: step.open / 138, // We divide by 138 because the number are very high
+    high: step.high / 138, // (and one of the lowest values we can find is 138)
+    low: step.low / 138,
+    close: step.close / 138
+  };
+}
+
+function scaleUp(step) { // denormalize
+  return {
+      open: step.open * 138,
+      high: step.high * 138,
+      low: step.low * 138,
+      close: step.close * 138
+  };
+}
+
+const scaledData = rawData.map(scaleDown);
+
+const trainingData = [
+  scaledData.slice(0, 5),
+  scaledData.slice(5, 10),
+  scaledData.slice(10, 15),
+  scaledData.slice(15, 20),
+];
+
+const net = new brain.recurrent.LSTMTimeStep({
+  inputSize: 4,
+  hiddenLayers: [8, 8],
+  outputSize: 4
+});
+
+net.train(trainingData, {
+  learningRate: 0.005,
+  errorThresh: 0.02,
+  // log: (stats) => console.log(stats)
+});
+
+// console.log(scaleUp(net.run(trainingData[0])));
+
+console.log(net.forecast([
+  trainingData[0][0],
+  trainingData[0][1],
+],3).map(scaleUp));
 
