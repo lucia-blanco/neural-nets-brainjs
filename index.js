42d45d3aef24b645b7a023661a76645003a02374
Lucia <lublasal@gmail.com>
2018-12-28 10:36:52 +0100

    Recurrent neural networks

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ console.log(Math.round(net.run([5,4,3])));*/
 
 ////////////// PREDICTING MULTIPLE STEPS ///////////////
 
-function scaleDown(step) { // normalize
+/*function scaleDown(step) { // normalize
   return {
     open: step.open / 138, // We divide by 138 because the number are very high
     high: step.high / 138, // (and one of the lowest values we can find is 138)
@@ -184,5 +184,57 @@ net.train(trainingData, {
 console.log(net.forecast([
   trainingData[0][0],
   trainingData[0][1],
-],3).map(scaleUp));
+],3).map(scaleUp));*/
 
+////////////// RECURRENT NEURAL NETWORK /////////
+const trainingData = [
+  '0+0=0',
+  '0+1=1',
+  '0+2=2',
+  '0+3=3',
+  '0+4=4',
+  '0+5=5',
+
+  '1+0=1',
+  '1+1=2',
+  '1+2=3',
+  '1+3=4',
+  '1+4=5',
+  '1+5=6',
+
+  '2+0=2',
+  '2+1=3',
+  '2+2=4',
+  '2+3=5',
+  '2+4=6',
+  '2+5=7',
+
+  '3+0=3',
+  '3+1=4',
+  '3+2=5',
+  '3+3=6',
+  '3+4=7',
+  '3+5=8',
+
+  '4+0=4',
+  '4+1=5',
+  '4+2=6',
+  '4+3=7',
+  '4+4=8',
+  '4+5=9',
+
+  '5+0=5',
+  '5+1=6',
+  '5+2=7',
+  '5+3=8',
+  '5+4=9',
+  '5+5=10',
+];
+
+const net = new brain.recurrent.LSTM({ hidden: [20] });
+
+net.train(trainingData, { errorThresh: 0.025 });
+
+console.log(net.run('0+1='));
+console.log(net.run('4+2='));
+console.log(net.run('2+5='));
